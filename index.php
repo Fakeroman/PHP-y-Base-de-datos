b50d865c784072fc1e193b69c15f5f6d744a5f96
Cristian Román Mancilla Torres <43075933+Fakeroman@users.noreply.github.com>
2019-05-31 20:19:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="connection.php" method="post">
+    Name: <input type="text" name="name"><br>
+    LastName: <input type="text" name="LastName"><br>
+    Age: <input type="number" name="Age"><br>
+    <input type="submit" value="">
+</form>
+</body>
+</html>
